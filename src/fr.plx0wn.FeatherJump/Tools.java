cb6b6d792e10f5d6df3479225190b7f764859857
Plx0wn <lucas.rouss@gmail.com>
2017-09-18 18:05:59 +0200

    Tools class

---
+++ src/fr.plx0wn.FeatherJump/Tools.java
@@ -0,0 +1,47 @@
+package fr.lucluc.featherjump;
+
+import java.lang.reflect.Constructor;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class Tools {
+
+	public static void scm(CommandSender sender, String msg) {
+		sender.sendMessage(ChatColor.translateAlternateColorCodes('&', msg));
+	}
+
+	public static void sendActionBar(Player player, String msg) {
+		String s = ChatColor.translateAlternateColorCodes('&', msg);
+		try {
+			Constructor<?> constructor = getNMSClass("PacketPlayOutChat")
+					.getConstructor(getNMSClass("IChatBaseComponent"), getNMSClass("ChatMessageType"));
+
+			Object icbc = getNMSClass("IChatBaseComponent").getDeclaredClasses()[0].getMethod("a", String.class)
+					.invoke(null, "{\"text\":\"" + s + "\"}");
+			Object packet = constructor.newInstance(icbc, getNMSClass("ChatMessageType").getEnumConstants()[2]);
+			Object entityPlayer = player.getClass().getMethod("getHandle").invoke(player);
+			Object playerConnection = entityPlayer.getClass().getField("playerConnection").get(entityPlayer);
+
+			playerConnection.getClass().getMethod("sendPacket", getNMSClass("Packet")).invoke(playerConnection, packet);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static Class<?> getNMSClass(String name) {
+		try {
+			return Class.forName("net.minecraft.server." + getVersion() + "." + name);
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+
+	public static String getVersion() {
+		return Bukkit.getServer().getClass().getPackage().getName().split("\\.")[3];
+	}
+
+}
